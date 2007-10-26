74bf34c2503444eac3291bea31479ff231edb865
bobbyrullo <bobbyrullo@92f3e887-6b3d-0410-b960-6bf569049b56>
2007-10-26 23:37:48 +0000

    added some support for generics. changed the utils package. br | 2006-01-05 15:55:51 -0800 (Thu, 05 Jan 2006)

---
+++ src/main/java/org/osaf/caldav4j/util/DateUtils.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2005 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osaf.caldav4j.util;
+
+import java.util.Calendar;
+
+import net.fortuna.ical4j.model.Date;
+import net.fortuna.ical4j.model.DateTime;
+import net.fortuna.ical4j.model.TimeZone;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class DateUtils {
+    private static final Log log = LogFactory.getLog(DateUtils.class);
+    
+    private static java.util.TimeZone J_TZ_GMT = TimeZone.getTimeZone("GMT");
+   
+    /**
+     * Creates an iCal4J DateTime. The values for year, month, day, hour,
+     * minutes, seconds and milliseconds should be set way that you specify them
+     * in a java.util.Calendar - which means zero indexed months for example
+     * (eg. January is '0').
+     * 
+     * Note that the TimeZone is not a java.util.TimeZone but an iCal4JTimeZone
+     * 
+     * @param year
+     * @param month
+     * @param day
+     * @param hour
+     * @param minutes
+     * @param seconds
+     * @param milliseconds
+     * @param tz
+     * @param utc
+     * @return
+     */
+    public static DateTime createDateTime(int year, int month, int day,
+            int hour, int minutes, int seconds, int milliseconds, TimeZone tz,
+            boolean utc) {
+        DateTime dateTime = new DateTime();
+        setFields(dateTime, year, month, day, hour, minutes, seconds,
+                milliseconds, tz, utc);
+        return dateTime;
+    }
+    
+    public static DateTime createDateTime(int year, int month, int day,
+            int hour, int minutes, TimeZone tz, boolean utc) {
+        DateTime dateTime = new DateTime();
+        setFields(dateTime, year, month, day, hour, minutes, 0, 0, tz, utc);
+        return dateTime;
+
+    }
+    
+    public static Date createDate(int year, int month, int day,
+            TimeZone tz, boolean utc) {
+        Date date = new Date();
+        setFields(date, year, month, day, 0, 0, 0, 0, tz, utc);
+        return date;
+
+    }
+    
+    private static void setFields(Date date, int year, int month, int day,
+            int hour, int minutes, int seconds, int milliseconds, TimeZone tz,
+            boolean utc){
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.setTimeZone(tz == null ? J_TZ_GMT : tz);
+        //calendar.
+        date.setTime(calendar.getTimeInMillis());
+        if (date instanceof DateTime){
+            if (utc) {
+               ((DateTime)date).setUtc(utc);
+            } else if (tz != null){
+                ((DateTime)date).setTimeZone(tz);
+            }
+            calendar.set(Calendar.HOUR, hour);
+            calendar.set(Calendar.MINUTE, minutes);
+            calendar.set(Calendar.SECOND, seconds);
+            calendar.set(Calendar.MILLISECOND, milliseconds);
+        }
+    }
+    
+}
---
+++ src/main/java/org/osaf/caldav4j/util/XMLUtils.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright 2005 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.osaf.caldav4j.util;
+
+import java.io.StringWriter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.xml.serialize.OutputFormat;
+import org.apache.xml.serialize.XMLSerializer;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.bootstrap.DOMImplementationRegistry;
+import org.w3c.dom.ls.DOMImplementationLS;
+import org.w3c.dom.ls.LSSerializer;
+
+public class XMLUtils {
+    private static final Log log = LogFactory.getLog(XMLUtils.class);
+    
+    private static DOMImplementation implementation = null;
+    
+    static {
+        try {
+            DOMImplementationRegistry registry = DOMImplementationRegistry
+                    .newInstance();
+            implementation = registry.getDOMImplementation("XML 3.0");
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Could not instantiate a DOMImplementation!", e);
+        }
+    }
+    
+    /**
+     * Creates a new xml DOM Document using a DOM 3.0 DOM Implementation
+     * 
+     * @param namespaceURI
+     *            the default XML Namespace for the document
+     * @param qualifiedName
+     *            the qualified name of the root element
+     * @return a new document
+     */
+    public static Document createNewDocument(String namespaceURI,
+            String qualifiedName) {
+
+        Document document = implementation.createDocument(namespaceURI,
+                qualifiedName, null);
+        return document;
+    }
+    
+    /**
+     * Serializes a DOM Document to XML 
+     * @param document a DOM document
+     * @return the Document serialized to XML
+     */
+    public static String toXML(Document document){
+        DOMImplementationLS domLS = (DOMImplementationLS) implementation;
+        LSSerializer serializer = domLS.createLSSerializer();
+        String s = serializer.writeToString(document);
+        
+        return s;
+    }    
+    
+    public static String toPrettyXML(Document document) {
+        StringWriter stringWriter = new StringWriter();
+        OutputFormat outputFormat = new OutputFormat(document, null, true);
+        XMLSerializer xmlSerializer = new XMLSerializer(stringWriter,
+                outputFormat);
+        xmlSerializer.setNamespaces(true);
+        try {
+            xmlSerializer.asDOMSerializer().serialize(document);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return stringWriter.toString();
+
+    }
+    
+    public static DOMImplementation getDOMImplementation(){
+        return implementation;
+    }
+    
+}
