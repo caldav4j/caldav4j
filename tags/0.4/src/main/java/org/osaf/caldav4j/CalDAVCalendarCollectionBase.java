1b3bbeb516c8830efe23603d811616bcb367a326
robipolli <robipolli@92f3e887-6b3d-0410-b960-6bf569049b56>
2008-08-05 12:45:05 +0000

    create 0.4 tag

---
+++ tags/0.4/src/main/java/org/osaf/caldav4j/CalDAVCalendarCollectionBase.java
@@ -0,0 +1,130 @@
+package org.osaf.caldav4j;
+
+/** 
+ * TODO tutti gli attributi, tutti i metodi non specializzati
+ * 
+ * @author rpolli
+ * */
+import java.util.Random;
+
+import net.fortuna.ical4j.model.Calendar;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HostConfiguration;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.webdav.lib.util.WebdavStatus;
+import org.osaf.caldav4j.cache.CalDAVResourceCache;
+import org.osaf.caldav4j.cache.NoOpResourceCache;
+import org.osaf.caldav4j.methods.CalDAV4JMethodFactory;
+import org.osaf.caldav4j.methods.PutMethod;
+import org.osaf.caldav4j.model.request.PropProperty;
+
+public abstract class CalDAVCalendarCollectionBase {
+
+	public static final PropProperty PROP_ETAG = new PropProperty(
+			CalDAVConstants.NS_DAV, "D", CalDAVConstants.PROP_GETETAG);
+	 CalDAV4JMethodFactory methodFactory = null;
+	 String calendarCollectionRoot = null;
+	 HostConfiguration hostConfiguration = null;
+	 String prodId = null;
+	 Random random = new Random();
+	 CalDAVResourceCache cache = NoOpResourceCache.SINGLETON;
+
+	//Configuration Methods
+	
+	public HostConfiguration getHostConfiguration() {
+	    return hostConfiguration;
+	}
+
+	public void setHostConfiguration(HostConfiguration hostConfiguration) {
+	    this.hostConfiguration = hostConfiguration;
+	}
+
+	public CalDAV4JMethodFactory getMethodFactory() {
+	    return methodFactory;
+	}
+
+	public void setMethodFactory(CalDAV4JMethodFactory methodFactory) {
+	this.methodFactory = methodFactory; }
+
+	protected String getCalendarCollectionRoot() {
+	    return calendarCollectionRoot;
+	}
+
+	public void setCalendarCollectionRoot(String path) {
+	    this.calendarCollectionRoot = path;
+	}
+
+	public CalDAVResourceCache getCache() {
+	    return cache;
+	}
+
+	public void setCache(CalDAVResourceCache cache) {
+	    this.cache = cache;
+	}
+
+	/**
+	 * Returns the path relative to the calendars path given an href
+	 * 
+	 * @param href
+	 * @return
+	 */
+	protected String getRelativePath(String href){
+	    int start = href.indexOf(calendarCollectionRoot);
+	    return href.substring(start + calendarCollectionRoot.length() + 1);
+	}
+
+	 PutMethod createPutMethodForNewResource(String resourceName,
+	        Calendar calendar) {
+	    PutMethod putMethod = methodFactory.createPutMethod();
+	    putMethod.setPath(calendarCollectionRoot + "/"
+	            + resourceName);
+	    putMethod.setAllEtags(true);
+	    putMethod.setIfNoneMatch(true);
+	    putMethod.setRequestBody(calendar);
+	    return putMethod;
+	}
+
+	 String getHref(String path){
+	    String href = hostConfiguration.getProtocol().getScheme() + "://"
+	    + hostConfiguration.getHost()
+	    + (hostConfiguration.getPort() != 80 ? ":" + hostConfiguration.getPort() : "")
+	    + ""
+	    + path;
+	    return href;
+	}
+
+	void put(HttpClient httpClient, Calendar calendar, String path,
+	        String etag)
+	        throws CalDAV4JException {
+	    PutMethod putMethod = methodFactory.createPutMethod();
+	    putMethod.addEtag(etag);
+	    putMethod.setPath(path);
+	    putMethod.setIfMatch(true);
+	    putMethod.setRequestBody(calendar);
+	    try {
+	        httpClient.executeMethod(hostConfiguration, putMethod);
+	        int statusCode = putMethod.getStatusCode();
+	        if (statusCode!= WebdavStatus.SC_NO_CONTENT
+	                && statusCode != WebdavStatus.SC_CREATED) {
+	            if (statusCode == WebdavStatus.SC_PRECONDITION_FAILED){
+	                throw new ResourceOutOfDateException(
+	                        "Etag was not matched: "
+	                                + etag);
+	            }
+	        }
+	    } catch (Exception e){
+	        throw new CalDAV4JException("Problem executing put method",e);
+	    }
+	
+	    Header h = putMethod.getResponseHeader("ETag");
+	
+	    if (h != null) {
+	        String newEtag = h.getValue();
+	        cache.putResource(new CalDAVResource(calendar, newEtag, getHref(path)));
+	    }
+	    
+	
+	}
+
+}
---
+++ tags/0.4/src/test/java/org/osaf/caldav4j/BaseTestCase.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2005 Open Source Applications Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.osaf.caldav4j;
+
+import java.io.InputStream;
+
+import junit.framework.TestCase;
+import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.model.Calendar;
+
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HostConfiguration;
+//import org.apache.commons.httpclient.HttpClient;
+import org.osaf.caldav4j.methods.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.InputStreamRequestEntity;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.webdav.lib.methods.DeleteMethod;
+import org.osaf.caldav4j.methods.CalDAV4JMethodFactory;
+import org.osaf.caldav4j.methods.MkCalendarMethod;
+import org.osaf.caldav4j.methods.PutMethod;
+
+
+/**
+ * Base class for CalDAV4j tests.
+ */
+public abstract class BaseTestCase
+    extends TestCase{
+    private static final Log log = LogFactory.getLog(BaseTestCase.class);
+    private HttpClient http = createHttpClient();
+    private HostConfiguration hostConfig = createHostConfiguration();
+
+    public static final String CALDAV_SERVER_HOST = "localhost";
+    public static final int CALDAV_SERVER_PORT = 8080;
+    public static final String CALDAV_SERVER_PROTOCOL = "http";
+    public static final String CALDAV_SERVER_WEBDAV_ROOT = "/chandler/dav/test/";
+    public static final String CALDAV_SERVER_USERNAME = "test";
+    public static final String CALDAV_SERVER_PASSWORD = "password";
+    public static final String CALDAV_SERVER_BAD_USERNAME = "IDONTEXIST";
+    public static final String COLLECTION      = "collection";
+
+    public static final String ICS_DAILY_NY_5PM = "Daily_NY_5pm.ics";
+    public static final String ICS_DAILY_NY_5PM_UID = "DE916949-731D-4DAE-BA93-48A38B2B2030";
+    public static final String ICS_DAILY_NY_5PM_SUMMARY = "Daily_NY_5pm";
+
+    public static final String ICS_ALL_DAY_JAN1 = "All_Day_NY_JAN1.ics";
+    public static final String ICS_ALL_DAY_JAN1_UID = "C68DADAD-37CE-44F7-8A37-52E1D02E29CA";
+
+    public static final String ICS_NORMAL_PACIFIC_1PM = "Normal_Pacific_1pm.ics";
+    public static final String ICS_NORMAL_PACIFIC_1PM_UID = "0F94FE7B-8E01-4B27-835E-CD1431FD6475";
+    public static final String ICS_NORMAL_PACIFIC_1PM_SUMMARY = "Normal_Pacific_1pm";
+
+    public static final String ICS_FLOATING_JAN2_7PM = "Floating_Jan_2_7pm.ics";
+    public static final String ICS_FLOATING_JAN2_7PM_SUMMARY = "Floating_Jan_2_7pm";
+    public static final String ICS_FLOATING_JAN2_7PM_UID = "0870D1E0-B17E-4875-85C5-2ABB02E27609";
+    
+    public static final String ICS_SINGLE_EVENT= "singleEvent.ics";
+
+    private CalDAV4JMethodFactory methodFactory = new CalDAV4JMethodFactory();
+    
+    public String getCalDAVServerHost() {
+        return CALDAV_SERVER_HOST;
+    }
+    
+    public int getCalDAVServerPort(){
+        return CALDAV_SERVER_PORT;
+    }
+    
+    public String getCalDavSeverProtocol(){
+        return CALDAV_SERVER_PROTOCOL;
+    }
+    
+    public String getCalDavSeverWebDAVRoot(){
+        return CALDAV_SERVER_WEBDAV_ROOT;
+    }
+    
+    public String getCalDavSeverUsername(){
+        return CALDAV_SERVER_USERNAME;
+    }
+    
+    public String getCalDavSeverPassword(){
+        return CALDAV_SERVER_PASSWORD;
+    }
+    
+    public HttpClient createHttpClient(){
+        HttpClient http = new HttpClient();
+
+        Credentials credentials = new UsernamePasswordCredentials(CALDAV_SERVER_USERNAME, CALDAV_SERVER_PASSWORD);
+        http.getState().setCredentials(
+        		new AuthScope(this.getCalDAVServerHost(), this.getCalDAVServerPort()),
+        		credentials);
+        http.getParams().setAuthenticationPreemptive(true);
+        return http;
+    }
+    
+    public HttpClient createHttpClientWithNoCredentials(){
+
+        HttpClient http = new HttpClient();
+        http.getParams().setAuthenticationPreemptive(true);
+        return http;
+    }
+    
+    public HostConfiguration createHostConfiguration(){
+        HostConfiguration hostConfig = new HostConfiguration();
+        hostConfig.setHost(getCalDAVServerHost(), getCalDAVServerPort());
+        return hostConfig;
+    }
+    
+    protected Calendar getCalendarResource(String resourceName) {
+        Calendar cal;
+
+        InputStream stream = this.getClass().getClassLoader()
+                .getResourceAsStream(resourceName);
+        CalendarBuilder cb = new CalendarBuilder();
+        
+        try {
+            cal = cb.build(stream);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        
+        return cal;
+    }    
+    
+    protected void put(String resourceFileName, String path){
+        PutMethod put = methodFactory.createPutMethod();
+        InputStream stream = this.getClass().getClassLoader()
+        .getResourceAsStream(resourceFileName);
+        put.setRequestEntity(new InputStreamRequestEntity(stream));
+        put.setPath(path);
+        try {
+            http.executeMethod(hostConfig, put);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    protected void del(String path){
+        DeleteMethod delete = new DeleteMethod();
+        delete.setPath(path);
+        try {
+        http.executeMethod(hostConfig, delete);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+    
+    protected void mkdir(String path){
+        MkCalendarMethod mk = new MkCalendarMethod();
+        mk.setPath(path);
+        try {
+        http.executeMethod(hostConfig, mk);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
