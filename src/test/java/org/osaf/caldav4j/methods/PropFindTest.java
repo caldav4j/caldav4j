3f44a7a79e185430476e82345592d8f11c10cbe0
robipolli@gmail.com <ioggstream@users.noreply.github.com>
2010-05-07 15:18:38 +0000

    propfind test

--- src/test/java/org/osaf/caldav4j/methods/PropFindTest.java
+++ src/test/java/org/osaf/caldav4j/methods/PropFindTest.java
@@ -7,11 +7,13 @@ import org.apache.commons.httpclient.HostConfiguration;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.osaf.caldav4j.model.response.Principal;
 import org.apache.webdav.lib.Ace;
 import org.apache.webdav.lib.BaseProperty;
 import org.apache.webdav.lib.Privilege;
 import org.apache.webdav.lib.Property;
 import org.apache.webdav.lib.PropertyName;
+import org.apache.webdav.lib.methods.AclMethod;
 import org.apache.webdav.lib.properties.AclProperty;
 import org.apache.webdav.lib.util.DOMUtils;
 import org.osaf.caldav4j.BaseTestCase;
@@ -21,6 +23,7 @@ import org.osaf.caldav4j.model.request.CalendarDescription;
 import org.osaf.caldav4j.model.request.DisplayName;
 import org.osaf.caldav4j.model.request.PropProperty;
 import org.osaf.caldav4j.model.util.PropertyFactory;
+import org.osaf.caldav4j.util.AceUtils;
 import org.osaf.caldav4j.util.XMLUtils;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -85,7 +88,7 @@ public class PropFindTest extends BaseTestCase {
 				AclProperty prop = (AclProperty) myEnum.nextElement();
 				NodeList nl = prop.getElement().getElementsByTagName("ace");
 				log.info(prop.getPropertyAsString());
-				Ace[] aces = prop.getAces();
+				Ace[] aces = (Ace[]) prop.getAces();
 				log.info(aces[0]);
 
 				log.info("There are aces # "+ nl.getLength() );
@@ -169,7 +172,7 @@ public class PropFindTest extends BaseTestCase {
 				log.info(e.getName());
 				AclProperty prop = (AclProperty) e;
 				log.info(prop.getPropertyAsString());
-				Ace[] aces = prop.getAces();
+				Ace[] aces = (Ace[]) prop.getAces();
 				log.info("There are aces # "+ aces.length );
 
 				for (int k=0; k<aces.length; k++) {
@@ -197,8 +200,9 @@ public class PropFindTest extends BaseTestCase {
 	
 	/**
 	 * TODO this test will work only on bedework which has a set of permission set
+	 * @throws CalDAV4JException 
 	 */
-	public void testNewPropfind() {
+	public void testNewPropfind() throws CalDAV4JException {
 		log.info("New Propfind");
 		HttpClient http = createHttpClient();
 		HostConfiguration hostConfig = createHostConfiguration();
@@ -226,24 +230,27 @@ public class PropFindTest extends BaseTestCase {
 			assertEquals(CALENDAR_DESCRIPTION, propfind.getCalendarDescription(COLLECTION_PATH));
 
 			// check that ACLs matches
-	    	AclProperty acls = propfind.getAcl(COLLECTION_PATH);
-	    	assertNotNull(acls);
-	    	log.info(acls.getPropertyAsString());
-			Ace[] aces = acls.getAces();
+			org.apache.webdav.lib.Ace[] aces = propfind.getAces(COLLECTION_PATH);
 			log.info("There are aces # "+ aces.length );
-
+			
 			for (int k=0; k<aces.length; k++) {
-				Ace ace = null;
-				ace = aces[k];
+				Ace ace = aces[k];
 				assertEquals("/user", ace.getInheritedFrom());
 				switch (k) {
 				case 0:
+					Principal pdav = AceUtils.getDavPrincipal(ace);
+					pdav.isOwner();
 					assertEquals("property", ace.getPrincipal());
 					assertEquals("owner", ace.getProperty().getLocalName());
-					Privilege p = (Privilege) ace.enumeratePrivileges().nextElement();
+					Privilege p = (Privilege) aces[k].enumeratePrivileges().nextElement();
 					assertEquals("all", p.getName());
 					break;
 
+				case 1:
+					assertEquals(CalDAVConstants.DAV_PRINCIPAL_AUTHENTICATED, ace.getPrincipal());
+					p = (Privilege) ace.enumeratePrivileges().nextElement();
+					assertTrue( p.getName().contains(CalDAVConstants.CALDAV_PRIVILEGE_READ_FREE_BUSY));
+					break;
 				default:
 					break;
 				}
@@ -259,11 +266,55 @@ public class PropFindTest extends BaseTestCase {
 		}
 
 	}
-	
+	public void testAclMethod() {
+		log.info("New Propfind");
+		HttpClient http = createHttpClient();
+		HostConfiguration hostConfig = createHostConfiguration();
+
+		AclMethod method = new AclMethod(COLLECTION_PATH);
+		
+		Ace ace;
+		ace = AceUtils.createAce(new Principal("owner"));
+		ace.addPrivilege(org.osaf.caldav4j.model.request.Privilege.SCHEDULE_DELIVER);
+		ace.addPrivilege(org.osaf.caldav4j.model.request.Privilege.WRITE);
+		ace.addPrivilege(org.osaf.caldav4j.model.request.Privilege.READ);
+		method.addAce(ace);
+		
+		
+		try {
+			http.executeMethod(hostConfig,method);
+			
+			
+			// verify output
+			PropFindMethod propfind = new PropFindMethod();
+			propfind.setPath(COLLECTION_PATH);
+			PropProperty propFindTag = new PropProperty(CalDAVConstants.NS_DAV,"D","propfind");
+			PropProperty aclTag = new PropProperty(CalDAVConstants.NS_DAV,"D","acl");
+			PropProperty propTag = new PropProperty(CalDAVConstants.NS_DAV,"D","prop");
+			propTag.addChild(aclTag);
+			propTag.addChild(new DisplayName());
+			propTag.addChild(new CalendarDescription());
+			propFindTag.addChild(propTag);
+			propfind.setPropFindRequest(propFindTag);
+			propfind.setDepth(0);
+			http.executeMethod(hostConfig,propfind);
+
+			log.info("post setacl returns: "+ propfind.getResponseBodyAsString());
+			// TODO check returned ACIS
+		} catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
 	@SuppressWarnings("unchecked")
 	private void printAce(Ace ace) {
-		PropProperty principal =	new PropProperty(CalDAVConstants.NS_DAV, "D", "property");
-		principal.addChild(new PropProperty(ace.getProperty().getNamespaceURI(), "D", ace.getProperty().getLocalName()));
+		PropProperty principal =	new PropProperty(CalDAVConstants.NS_DAV, CalDAVConstants.NS_QUAL_DAV, "property");
+		principal.addChild(new PropProperty(ace.getProperty().getNamespaceURI(), CalDAVConstants.NS_QUAL_DAV, ace.getProperty().getLocalName()));
 		String stringFormattedAci = String.format("ACE:" + 
 				" principal: %s ", "property".equals(ace.getPrincipal()) ? XMLUtils.prettyPrint(principal)  : ace.getPrincipal() +
 						" ereditata da: " + ace.getInheritedFrom() + ";" );
@@ -272,7 +323,7 @@ public class PropFindTest extends BaseTestCase {
 		log.debug("privileges are" );
 		while (privs.hasMoreElements()) {
 			Privilege priv = privs.nextElement();						
-			log.debug(String.format("<privilege><%s:%s/></privilege>",  priv.getNamespace(), priv.getName() ));
+			log.debug(String.format("<privilege><%s %s/></privilege>",  priv.getNamespace(), priv.getName() ));
 		}
 
 	}
