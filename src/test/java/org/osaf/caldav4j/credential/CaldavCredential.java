5abf14a4411a5b54fb80761510703ac41b6fccc4
robipolli@gmail.com <ioggstream@users.noreply.github.com>
2011-02-15 16:38:17 +0000

    added chandler hub credential for easy testing

--- src/test/java/org/osaf/caldav4j/credential/CaldavCredential.java
+++ src/test/java/org/osaf/caldav4j/credential/CaldavCredential.java
@@ -1,20 +1,38 @@
 package org.osaf.caldav4j.credential;
 
+import java.net.URI;
+
 public class CaldavCredential {
+	static String CALDAV_YAHOO = "https://caldav.calendar.yahoo.com/dav/%s/Calendar/";
+	static String CALDAV_GOOGLE = "https://www.google.com/calendar/dav/%s/";
+	static String CALDAV_BEDEWORK = "http://revolver:8080/ucaldav/user/%s/";
+	static String CALDAV_CHANDLER = "https://hub.chandlerproject.org/dav/%s/";
+
 	// sample class for storing credentials 
     //public static final String CALDAV_SERVER_HOST = "10.0.8.205";
-    public  String host = "localhost";
-    public  int port = 8080;
-    public  String protocol = "http";
-    public  String user = "vbede";
-    public  String home = "/ucaldav/user/"+user+"/";
-    public  String password = "bedework";
+    public  String host = "hub.chandlerproject.org";
+    public  int port = 443;
+    public  String protocol = "https";
+    public  String user = "caldav4j";
+    public  String home = "/dav/"+user+"/";
+    public  String password = "CalDAV4J";
     public  String collection      = "collection/";
 
     public CaldavCredential() {
     	
     }
     
+    public CaldavCredential(String uri) {
+    	try {
+			URI server = new URI(uri);
+			protocol = server.getScheme().replaceAll("://", "");
+			host = server.getHost();
+			port = server.getPort() != -1 ? server.getPort() : (server.getScheme().endsWith("s") ? 443: 80);
+			home = server.getPath().replace("\\w+/$", "");
+		} catch (Exception e) {
+			// noop
+		}
+    }
     public CaldavCredential(String proto, String server, int  port, String base, String collection, String user, String pass) {
     	this.host = server;
     	this.port = port;
