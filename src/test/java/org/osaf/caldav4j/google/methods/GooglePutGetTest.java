8bd840963934c599014555706b9128d9c87e12ff
robipolli@gmail.com <ioggstream@users.noreply.github.com>
2011-02-18 18:01:18 +0000

    fixed simple event

--- src/test/java/org/osaf/caldav4j/google/methods/GooglePutGetTest.java
+++ src/test/java/org/osaf/caldav4j/google/methods/GooglePutGetTest.java
@@ -4,8 +4,14 @@
 package org.osaf.caldav4j.google.methods;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.component.VEvent;
+import net.fortuna.ical4j.model.property.DtStamp;
 
 import org.apache.commons.httpclient.HostConfiguration;
 import org.apache.commons.logging.Log;
@@ -24,7 +30,6 @@ import org.osaf.caldav4j.util.CaldavStatus;
 import org.osaf.caldav4j.util.ICalendarUtils;
 
 // TODO: fix; see issue 47
-@Ignore
 public class GooglePutGetTest extends BaseTestCase {
 
 	private static final Log log = LogFactory.getLog(GooglePutGetTest.class);
@@ -35,7 +40,7 @@ public class GooglePutGetTest extends BaseTestCase {
 	 caldavCredential = new GCaldavCredential();
 	}
 
-    
+    private List<String> addedEvents = new ArrayList<String>();
 
     
     @Before
@@ -48,32 +53,31 @@ public class GooglePutGetTest extends BaseTestCase {
     @After
     public void tearDown() throws Exception {
         super.tearDown();
-        del(COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM);
+        for (String s: addedEvents) {
+        	del(COLLECTION_PATH + "/" + s);	
+        }
+        
         //del(COLLECTION_PATH);
     }
 	@Test
-    public void testAddRemoveCalendarResource() throws Exception{
+	@Ignore // Google doesn't allow deletion of recurring events
+    public void testAddRemoveRecurringCalendarResource() throws Exception{
         HttpClient http = createHttpClient();
         HostConfiguration hostConfig = createHostConfiguration();
 
         Calendar cal = getCalendarResource(BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM_PATH);
-        if (cal == null) {
-        	throw new Exception("can't find in CLASSPATH:" 
-        			+ BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM_PATH);
-        }
         PutMethod put = methodFactory.createPutMethod();
         put.setIfNoneMatch(true);
         put.setAllEtags(true);
         put.setRequestBody(cal);
-        COLLECTION_PATH = COLLECTION_PATH.replaceAll("/+", "/");
         
         log.info("putting " + COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM);
         put.setPath(COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM);
         http.executeMethod(hostConfig, put);
         int statusCode = put.getStatusCode();
         // google used SC_NO_CONTENT instead of SC_CREATED
-        assertEquals("Status code for put:", CaldavStatus.SC_CREATED, statusCode);
-
+        assertEquals("Status code for put" + put.getResponseBodyAsString(), CaldavStatus.SC_CREATED, statusCode);
+        addedEvents.add(BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM);
 
         //ok, so we created it...let's make sure it's there!
         GetMethod get = methodFactory.createGetMethod();
@@ -92,6 +96,7 @@ public class GooglePutGetTest extends BaseTestCase {
         put = methodFactory.createPutMethod();
         put.setIfNoneMatch(true);
         put.setAllEtags(true);
+        ICalendarUtils.addOrReplaceProperty(cal.getComponent(Component.VEVENT), new DtStamp());
         put.setRequestBody(cal);
         put.setPath(COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_DAILY_NY_5PM);
         http.executeMethod(hostConfig, put);
@@ -102,5 +107,51 @@ public class GooglePutGetTest extends BaseTestCase {
                 CaldavStatus.SC_CONFLICT, statusCode);  
    }
     
+	@Test
+    public void testAddRemoveCalendarResource() throws Exception{
+        HttpClient http = createHttpClient();
+        HostConfiguration hostConfig = createHostConfiguration();
+
+        Calendar cal = getCalendarResource(BaseTestCase.ICS_GOOGLE_ALL_DAY_JAN1_PATH);
+        PutMethod put = methodFactory.createPutMethod();
+        put.setIfNoneMatch(true);
+        put.setAllEtags(true);
+        put.setRequestBody(cal);
+        
+        log.info("putting " + COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_ALL_DAY_JAN1);
+        put.setPath(COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_ALL_DAY_JAN1);
+        http.executeMethod(hostConfig, put);
+        int statusCode = put.getStatusCode();
+        // google used SC_NO_CONTENT instead of SC_CREATED
+        assertEquals("Status code for put" + put.getResponseBodyAsString(), CaldavStatus.SC_CREATED, statusCode);
+        addedEvents.add(BaseTestCase.ICS_GOOGLE_ALL_DAY_JAN1);
+
+        //ok, so we created it...let's make sure it's there!
+        GetMethod get = methodFactory.createGetMethod();
+        get.setPath(COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_ALL_DAY_JAN1);
+        http.executeMethod(hostConfig, get);
+        statusCode = get.getStatusCode();
+        assertEquals("Status code for get: ", CaldavStatus.SC_OK, statusCode);
+        
+        //now let's make sure we can get the resource body as a calendar
+        Calendar calendar = get.getResponseBodyAsCalendar();
+        VEvent event = ICalendarUtils.getFirstEvent(calendar);
+        String uid = ICalendarUtils.getUIDValue(event);
+        assertEquals(ICS_GOOGLE_ALL_DAY_JAN1_UID, uid);
+        
+        //let's make sure that a subsequent put with "if-none-match: *" fails
+        put = methodFactory.createPutMethod();
+        put.setIfNoneMatch(true);
+        put.setAllEtags(true);
+        ICalendarUtils.addOrReplaceProperty(cal.getComponent(Component.VEVENT), new DtStamp());
+        put.setRequestBody(cal);
+        put.setPath(COLLECTION_PATH + "/" + BaseTestCase.ICS_GOOGLE_ALL_DAY_JAN1);
+        http.executeMethod(hostConfig, put);
+        statusCode = put.getStatusCode();
+
+      //was CaldavStatus.SC_PRECONDITION_FAILED but gcalendar doesn't support if-tag and preconditions
+        assertEquals("Status code for put:",
+                CaldavStatus.SC_CONFLICT, statusCode);  
+   }
 
 }
