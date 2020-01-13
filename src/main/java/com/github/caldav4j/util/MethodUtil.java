2c714a9004c0c6d0a55a56160c83fd7f23f721b2
Alejandro Marín Buriticá <alejo.enix@gmail.com>
2020-01-13 06:08:25 -0500

    Remove log to avoid exception due to closed content stream of response entity.

--- src/main/java/com/github/caldav4j/util/MethodUtil.java
+++ src/main/java/com/github/caldav4j/util/MethodUtil.java
@@ -4,18 +4,12 @@ import com.github.caldav4j.exceptions.*;
 import com.github.caldav4j.exceptions.ResourceNotFoundException.IdentifierType;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 /**
  * Method Utilities
  */
 public class MethodUtil {
-	private static final Logger log = LoggerFactory.getLogger(MethodUtil.class);
-	
+
 	/**
 	 * Throws various exceptions depending on the status &gt;= 400 of the given method
 	 * @param method Method causing error
@@ -26,14 +20,7 @@ public class MethodUtil {
 	public static int StatusToExceptions(HttpRequestBase method, HttpResponse response) throws CalDAV4JException {
 		if (method != null && response != null) {
 			int status = response.getStatusLine().getStatusCode();
-			if (log.isDebugEnabled()) {
-				try {
-					log.debug("Server returned " + EntityUtils.toString(response.getEntity(),"UTF-8"));
-				} catch (IOException e) {
-					throw new CalDAV4JException("Error retrieving server response", e);
-				}
-			}
-			if (status >= 300) {				
+			if (status >= 300) {
 				switch (status) {
 				case CalDAVStatus.SC_CONFLICT:
 					throw new ResourceOutOfDateException("Conflict accessing: " + method.getURI() );
